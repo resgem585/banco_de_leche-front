11a48ce57114ef07764c26abf95f80cfa1cb159e
Rodolfo <resgem585@gmail.com>
2024-09-20 19:03:22 -0600

    se agregaron campos restantes para edit donante

--- src/components/EditDonante.jsx
+++ src/components/EditDonante.jsx
@@ -54,12 +54,12 @@ function EditDonante() {
   // Manejador para enviar los datos del formulario
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     try {
       await updateDonante({
         variables: {
-          id: id,  // Cambiado a `id` en lugar de `_id`
-          input: {  // Asegúrate de que estás pasando los datos correctamente en el objeto `input`
+          id: id,
+          input: {
             tipo: donanteData.tipo,
             firstName: donanteData.firstName,
             lastName: donanteData.lastName,
@@ -82,7 +82,7 @@ function EditDonante() {
           }
         }
       });
-  
+
       setUpdateSuccess(true);  // Mostrar el mensaje de éxito
     } catch (error) {
       console.error("Error updating donante:", error);
@@ -130,6 +130,233 @@ function EditDonante() {
       </div>
 
       {/* Otros campos siguen... */}
+      <div className="row form-group mb-2">
+        <div className="col">
+          <label htmlFor="lastName">Apellido</label>
+          <input
+            type="text"
+            className="form-control"
+            id="lastName"
+            name="lastName"
+            value={donanteData.lastName}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="edad">Edad</label>
+          <input
+            type="number"
+            className="form-control"
+            id="edad"
+            name="edad"
+            value={donanteData.edad}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <div className="row form-group mb-2">
+        <div className="col">
+          <label htmlFor="direccion">Dirección</label>
+          <input
+            type="text"
+            className="form-control"
+            id="direccion"
+            name="direccion"
+            value={donanteData.direccion}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="ocupacion">Ocupación</label>
+          <input
+            type="text"
+            className="form-control"
+            id="ocupacion"
+            name="ocupacion"
+            value={donanteData.ocupacion}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <div className="row form-group mb-2">
+        <div className="col">
+          <label htmlFor="partos">Partos</label>
+          <input
+            type="number"
+            className="form-control"
+            id="partos"
+            name="partos"
+            value={donanteData.partos}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="cesareas">Cesáreas</label>
+          <input
+            type="number"
+            className="form-control"
+            id="cesareas"
+            name="cesareas"
+            value={donanteData.cesareas}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <div className="row form-group mb-2">
+        <div className="col">
+          <label htmlFor="apellidosRNLactante">Apellidos RN Lactante</label>
+          <input
+            type="text"
+            className="form-control"
+            id="apellidosRNLactante"
+            name="apellidosRNLactante"
+            value={donanteData.apellidosRNLactante}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="sdg">SDG</label>
+          <input
+            type="number"
+            className="form-control"
+            id="sdg"
+            name="sdg"
+            value={donanteData.sdg}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <div className="row form-group mb-2">
+        <div className="col">
+          <label htmlFor="fechaNacimRN">Fecha de Nacimiento RN</label>
+          <input
+            type="date"
+            className="form-control"
+            id="fechaNacimRN"
+            name="fechaNacimRN"
+            value={donanteData.fechaNacimRN}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="complicacionesEmbarazo">Complicaciones de Embarazo</label>
+          <input
+            type="text"
+            className="form-control"
+            id="complicacionesEmbarazo"
+            name="complicacionesEmbarazo"
+            value={donanteData.complicacionesEmbarazo}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <div className="row form-group mb-2">
+        <div className="col">
+          <label htmlFor="transfusionesUltimos5Anos">Transfusiones Últimos 5 Años</label>
+          <select
+            className="form-select"
+            id="transfusionesUltimos5Anos"
+            name="transfusionesUltimos5Anos"
+            value={donanteData.transfusionesUltimos5Anos}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione una opción</option>
+            <option value="Sí">Sí</option>
+            <option value="No">No</option>
+          </select>
+        </div>
+        <div className="col">
+          <label htmlFor="tatuajesPiercingsAcupunturaUltimoAno">Tatuajes/Piercings/Acupuntura Último Año</label>
+          <select
+            className="form-select"
+            id="tatuajesPiercingsAcupunturaUltimoAno"
+            name="tatuajesPiercingsAcupunturaUltimoAno"
+            value={donanteData.tatuajesPiercingsAcupunturaUltimoAno}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione una opción</option>
+            <option value="Sí">Sí</option>
+            <option value="No">No</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="row form-group mb-2">
+        <div className="col">
+          <label htmlFor="tratamientoMedico">Tratamiento Médico</label>
+          <input
+            type="text"
+            className="form-control"
+            id="tratamientoMedico"
+            name="tratamientoMedico"
+            value={donanteData.tratamientoMedico}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="pruebaRapidaSifilis">Prueba Rápida Sífilis</label>
+          <select
+            className="form-select"
+            id="pruebaRapidaSifilis"
+            name="pruebaRapidaSifilis"
+            value={donanteData.pruebaRapidaSifilis}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione una opción</option>
+            <option value="No Reactivo">No Reactivo</option>
+            <option value="Reactivo">Reactivo</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="row form-group mb-2">
+        <div className="col">
+          <label htmlFor="pruebaRapidaVIH">Prueba Rápida VIH</label>
+          <select
+            className="form-select"
+            id="pruebaRapidaVIH"
+            name="pruebaRapidaVIH"
+            value={donanteData.pruebaRapidaVIH}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione una opción</option>
+            <option value="No Reactivo">No Reactivo</option>
+            <option value="Reactivo">Reactivo</option>
+          </select>
+        </div>
+        <div className="col">
+          <label htmlFor="pruebaRapidaHepatitisC">Prueba Rápida Hepatitis C</label>
+          <select
+            className="form-select"
+            id="pruebaRapidaHepatitisC"
+            name="pruebaRapidaHepatitisC"
+            value={donanteData.pruebaRapidaHepatitisC}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione una opción</option>
+            <option value="No Reactivo">No Reactivo</option>
+            <option value="Reactivo">Reactivo</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="row form-group mb-2">
+        <div className="col">
+          <label htmlFor="observaciones">Observaciones</label>
+          <textarea
+            className="form-control"
+            id="observaciones"
+            name="observaciones"
+            value={donanteData.observaciones}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
 
       <div className="row form-group mb-2">
         <div className="col">
